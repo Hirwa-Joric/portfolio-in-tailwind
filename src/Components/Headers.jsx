b73a4efee5ee9b1229062f4c3b1932b06f6a843d
Hirwa-Joric <hirwajoric@gmail.com>
2023-06-08 20:12:20 +0200

    updated

--- src/Components/Headers.jsx
+++ src/Components/Headers.jsx
@@ -7,7 +7,8 @@ function Headers() {
                 <p className="font-srif font-normal font-semibold text-6xl leading-[136.7%] text-[#284D57]">
                     Performance- <br></br>based <span className="text-[#E64B2F]">businesses</span> succeed.
                 </p>
-                <p>Small is a dedicated performance  managment platform that helps you execute yourstrategic goals and  retail  key talent.</p>
+                <p className="font-Montserrat font-normal text-base leading-[136.7%] w-[567px] h-[50px] text-[#5B5653] ">
+                    Small is a dedicated performance  management platform that helps you execute nonstrategic goals and  retail  key talent.</p>
                 
             </div>
         </div>
