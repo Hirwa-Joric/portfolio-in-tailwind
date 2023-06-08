c5ddf1fb4c1d7773fbbf83516cbe5c5e2029e9b0
Hirwa-Joric <hirwajoric@gmail.com>
2023-06-08 20:12:20 +0200

    updated

--- src/Components/Headers.jsx
+++ src/Components/Headers.jsx
@@ -1,7 +1,17 @@
 import Navbar from "./Navbar"
 function Headers() {
     return (
-      <Navbar/>
+        <div className="w-[1440px] h-[830px] m-auto bg-slate-400">
+            <Navbar />
+            <div className="w-[680px] h-[327px]">
+                <p className="font-srif font-normal font-semibold text-6xl leading-[136.7%] text-[#284D57]">
+                    Performance- <br></br>based <span className="text-[#E64B2F]">businesses</span> succeed.
+                </p>
+                <p>Small is a dedicated performance  managment platform that helps you execute yourstrategic goals and  retail  key talent.</p>
+                
+            </div>
+        </div>
+
   )
 }
 
--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,13 +2,13 @@ import Group from "../assets/Group.svg";
 import text from "../assets/text.svg"
 
 function Navbar() {
-    return (<div className="flex gap-[60px] align-middle justify-between w-[1112px] h-[52px] top-[38px] left=[164px] position">
+    return (<div className="flex gap-[70px] align-center justify-center  h-[52px] left=[164px] ] ">
         <div className = "flex gap-3">
             <img src={Group} alt="logo" />
             <img src={text} alt="text-logo" />
         </div>
         <div className="flex">
-            <ul className="flex gap-5">
+            <ul className="flex gap-5 align-center items-center ">
                 <li>Product</li>
                 <li>About</li>
                 <li>Pricing</li>
@@ -18,10 +18,13 @@ function Navbar() {
         </div>
         <div className="flex gap-[5px]">
             <div
-                className="text-center w-[96px] h-[43px] "
+                className=" flex items-center justify-center text-center w-[96px] h-[43px] border border-1 border-solid border-gray-300 rounded-[35px]"
             >
                 Sign in</div>
-            <div>Let's talk</div>
+            <div
+                className=" flex items-center justify-center text-center w-[96px] h-[43px] border border-1 border-solid bg-[#E64B2F] rounded-[35px] text-white"
+            >
+                Let's talk</div>
         </div>
     </div>
 )
